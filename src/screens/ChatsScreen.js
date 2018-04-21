2f3831423f64de0bae07a70187b29aa1bdf651e6
vnovick <vnovick@gmail.com>
2018-04-21 04:31:15 +0300

    step 2 initial commit

--- src/screens/ChatsScreen.js
+++ src/screens/ChatsScreen.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, ImageBackground, StyleSheet, FlatList } from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { getChats } from '../services/api';
 
 export class ChatsScreen extends Component {
-  render() {
+
+  state = {
+    chats: []
+  }
+
+  componentDidMount() {
+    getChats().then((chats) => {
+      this.setState({
+        chats
+      })
+    });
+  }
+
+  getChatItem(item){
     return (
       <View>
-        <Text> ChatScreen </Text>
+        <Text>{item.title}</Text>
       </View>
     )
   }
+
+  render() {
+    return (
+      <ImageBackground source={require('../assets/imgs/background.png')} style={styles.container}>
+        <FlatList
+          style={styles.container}
+          data={this.state.chats}
+          renderItem={({ item }) => this.getChatItem(item)}
+          keyExtractor={(item, index) => (`message-${index}`)}
+        />
+      </ImageBackground>
+    )
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%'
+  }
+})
---
+++ src/services/api.js
@@ -0,0 +1,76 @@
+export const mockMessages = [
+  {
+      incoming: true,
+      message: 'Hi Vladimir',
+  },
+  {
+      incoming: false,
+      message: 'Hi, John',
+  },
+  {
+      incoming: true,
+      message: 'When we will learn real data fetching?'
+  },
+  {
+      incoming: false,
+      message: `Let's get the basic first. We need to understand styling and handling inputs right? After all you want not only to receive but send messages too right?`,
+  }
+]
+
+export const chats = [{
+  id: "123",
+  title: "John",
+  description: "Hey there",
+  avatar: "http://www.codetic.net/demo/templates/Privado/img/avatar.png",
+  messages: mockMessages
+},{
+  id: "123",
+  title: "John",
+  description: "Hey there",
+  avatar: "http://www.codetic.net/demo/templates/Privado/img/avatar.png",
+  messages: mockMessages
+},{
+  id: "123",
+  title: "John",
+  description: "Hey there",
+  avatar: "http://www.codetic.net/demo/templates/Privado/img/avatar.png",
+  messages: mockMessages
+},{
+  id: "123",
+  title: "John",
+  description: "Hey there",
+  avatar: "http://www.codetic.net/demo/templates/Privado/img/avatar.png",
+  messages: mockMessages
+},{
+  id: "123",
+  title: "John",
+  description: "Hey there",
+  avatar: "http://www.codetic.net/demo/templates/Privado/img/avatar.png",
+  messages: mockMessages
+},{
+  id: "123",
+  title: "John",
+  description: "Hey there",
+  avatar: "http://www.codetic.net/demo/templates/Privado/img/avatar.png",
+  messages: mockMessages
+},{
+  id: "123",
+  title: "John",
+  description: "Hey there",
+  avatar: "http://www.codetic.net/demo/templates/Privado/img/avatar.png",
+  messages: mockMessages
+},{
+  id: "123",
+  title: "John",
+  description: "Hey there",
+  avatar: "http://www.codetic.net/demo/templates/Privado/img/avatar.png",
+  messages: mockMessages
+}]
+
+export const getChats = () => (
+  new Promise(resolve => setTimeout(() => resolve(chats), 1000))
+)
+
+export const getMessagesById = (id) => {
+  new Promise(resolve => setTimeout(() => resolve(mockMessages), 1000))
+}
